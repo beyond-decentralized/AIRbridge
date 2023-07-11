57cd2e53ed8e4d1813873ff4df489a1bddf0138a
Artem Shamsutdinov <Artem.V.Shamsutdinov@dataindependence.net>
2023-07-11 05:20:00 -0600

    added click-back to main UI

--- UI/react/main/src/Layout.tsx
+++ UI/react/main/src/Layout.tsx
@@ -1,13 +1,13 @@
-import { Redirect, Route, useLocation } from 'react-router-dom';
+import { Redirect, Route, useLocation, useHistory } from 'react-router-dom';
 import { apps, briefcase, desktop } from 'ionicons/icons';
 
 import {
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs
+    IonIcon,
+    IonLabel,
+    IonRouterOutlet,
+    IonTabBar,
+    IonTabButton,
+    IonTabs
 } from '@ionic/react';
 import { useEffect, useState } from 'react';
 
@@ -20,51 +20,62 @@ import UIsPage from './pages/UIsPage';
 import './Layout.css';
 
 const Layout: React.FC = () => {
+    const history = useHistory()
     const location = useLocation()
     const [hideTabBar, setHideTabBar] = useState<boolean>()
 
     let hideTabBarClassName = ''
-    if(hideTabBar) {
+    if (hideTabBar) {
         hideTabBarClassName = 'hide-tab-bar'
     }
-    
+
     useEffect(() => {
         let hideTabBar = location.pathname.startsWith('/ui')
         setHideTabBar(hideTabBar)
     }, [location]);
 
+    function handleTurbaseLogoClick() {
+        history.push("/applications")
+    }
+
     return (
-        <IonTabs>
-            <IonRouterOutlet>
-                <Route exact path="/rootRepositories">
-                    <RootRepositoriesPage />
-                </Route>
-                <Route exact path="/repository/:repositoryId">
-                    <RepositoryPage />
-                </Route>
-                <Route exact path="/applications">
-                    <AppsPage />
-                </Route>
-                <Route exact path="/user-interfaces">
-                    <UIsPage />
-                </Route>
-                <Route exact path="/">
-                    <Redirect to="/rootRepositories" />
-                </Route>
-                <Route exact path="/ui">
-                    <UIPage />
-                </Route>
-            </IonRouterOutlet>
-            <IonTabBar id="tab-bar" className={hideTabBarClassName} slot="bottom">
-                <IonTabButton tab="repositories" href="/rootRepositories">
-                    <IonIcon icon={briefcase} />
-                    <IonLabel>Repositories</IonLabel>
-                </IonTabButton>
-                <IonTabButton tab="applications" href="/applications">
-                    <IonIcon icon={apps} />
-                    <IonLabel>Apps</IonLabel>
-                </IonTabButton>
-                {/*
+        <>
+            {hideTabBar && <img
+                className="turbase-button"
+                onClick={handleTurbaseLogoClick}
+                src="assets/turbase-turbine.png"
+            ></img>}
+            <IonTabs>
+                <IonRouterOutlet>
+                    <Route exact path="/rootRepositories">
+                        <RootRepositoriesPage />
+                    </Route>
+                    <Route exact path="/repository/:repositoryId">
+                        <RepositoryPage />
+                    </Route>
+                    <Route exact path="/applications">
+                        <AppsPage />
+                    </Route>
+                    <Route exact path="/user-interfaces">
+                        <UIsPage />
+                    </Route>
+                    <Route exact path="/">
+                        <Redirect to="/rootRepositories" />
+                    </Route>
+                    <Route exact path="/ui">
+                        <UIPage />
+                    </Route>
+                </IonRouterOutlet>
+                <IonTabBar id="tab-bar" className={hideTabBarClassName} slot="bottom">
+                    <IonTabButton tab="repositories" href="/rootRepositories">
+                        <IonIcon icon={briefcase} />
+                        <IonLabel>Repositories</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab="applications" href="/applications">
+                        <IonIcon icon={apps} />
+                        <IonLabel>Apps</IonLabel>
+                    </IonTabButton>
+                    {/*
   Need to implement search for user interfaces based on the
   schemas of the currently loaded repositories
   <IonTabButton tab="user-interfaces" href="/user-interfaces">
@@ -72,12 +83,13 @@ const Layout: React.FC = () => {
     <IonLabel>UIs</IonLabel>
   </IonTabButton>
 */}
-                <IonTabButton tab="curret-user-interface" href="/ui">
-                    <IonIcon icon={desktop} />
-                    <IonLabel>UI</IonLabel>
-                </IonTabButton>
-            </IonTabBar>
-        </IonTabs>
+                    <IonTabButton tab="curret-user-interface" href="/ui">
+                        <IonIcon icon={desktop} />
+                        <IonLabel>UI</IonLabel>
+                    </IonTabButton>
+                </IonTabBar>
+            </IonTabs>
+        </>
     )
 }
 
